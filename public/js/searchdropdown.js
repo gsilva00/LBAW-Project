532bf37283a9cf9082fddc0965d2d8e8e98dd1b2
Guilherme Silva <gmpas4444@gmail.com>
2024-11-21 23:26:29 +0000

    Fixing merging errors
    needs more testing

--- public/js/searchdropdown.js
+++ public/js/searchdropdown.js
@@ -6,11 +6,6 @@ document.addEventListener('DOMContentLoaded', function() {
         searchDropdownMenu.classList.toggle('show');
     });
 
-    document.getElementById('add-filters-button').addEventListener('click', function() {
-        // Logic to show or hide filter options
-        alert('Add filter options here');
-    });
-
     // Close the dropdown if clicked outside
     window.addEventListener('click', function(event) {
         if (!searchButton.contains(event.target) && !searchDropdownMenu.contains(event.target)) {
--- resources/views/pages/header.blade.php
+++ resources/views/pages/header.blade.php
@@ -1,56 +1,49 @@
 <header>
-        <div id="top-part-header">
-            <h1><a href="{{ route('homepage') }}" class="logo"> {{ config('app.name', 'Laravel') }}</a></h1>
-            <h2><i class='bx bx-heart'></i> Followed Authors' News</h2>
-            <h2><i class='bx bx-purchase-tag'></i> Followed Tags</h2>
-            <h2><i class='bx bx-book'></i> Followed Topics</h2>
-            <div id="profile" class="dropdown">
-                
-                    @if(Auth::check())
-                        <button type="button" id="profile-button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class='bx bx-user-circle'></i>
-                            <h2>{{$username}}</h2>
-                        </button>
-                        <div class="dropdown-menu" aria-labelledby="profile-button">
-                            <a class="dropdown-item" href="{{ route('profile', ['username' => $username]) }}"><h2>See Profile</h2></a>
-                            <a class="dropdown-item" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><h2>Logout</h2></a>
-                            <a class="dropdown-item" href="#"><h2>Something for admin</h2></a>
-                        </div>
-                        <form id="logout-form" action="{{ route('logout') }}" method="GET" style="display: none;">
-                        @csrf
-                    </form>
-                    @else
-                    <a id="profile-button" href="{{ route('login') }}">
-                        <i class='bx bx-user-circle'></i><h2>Login</h2>
-                    </a>
-                @endif
-            </div>  <!-- Needs to be change to get login/logout -->
-        </div>
-        <div id="bottom-part-header">
-            <a href="{{ route('homepage') }}"><h2><i class='bx bx-home-alt'></i> Homepage</h2></a>
-            <a href="{{ route('recentnews.show') }}"><h2><i class='bx bx-stopwatch'></i>Most Recent News</h2></a>
-            <a href="{{ route('votednews.show') }}"><h2><i class='bx bx-sort'></i> Most Voted News</h2></a>
-            <h2><i class='bx bx-trending-up'></i>Trending Tags</h2>
-            <h2 class="topic">
-                <a href="{{ route('search.show', ['topics' => ['Politics']]) }}">Politics</a>
-            </h2>
-            <h2 class="topic">
-                <a href="{{ route('search.show', ['topics' => ['Business']]) }}">Business</a>
-            </h2>
-            <h2 class="topic">
-                <a href="{{ route('search.show', ['topics' => ['Technology']]) }}">Technology</a>
-            </h2>
-            <h2 class="topic">
-                <a href="{{ route('search.show', ['topics' => ['Science']]) }}">Science</a>
-            </h2>
-            <h2><i class='bx bx-news'></i>All Topics</h2>
-            <button type="button" id="search-button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class='bx bx-search'></i>
-            </button>
-            <div class="dropdown-menu" id="search-menu" aria-labelledby="search-button">
-                <form class="dropdown-item" action="{{ route('search.show') }}" method="GET">
-                    @include('partials.search_form')
+    <div id="top-part-header">
+        <h1><a href="{{ route('homepage') }}" class="logo"> {{ config('app.name', 'Laravel') }}</a></h1>
+        <h2><i class='bx bx-heart'></i> Followed Authors' News</h2>
+        <h2><i class='bx bx-purchase-tag'></i> Followed Tags</h2>
+        <h2><i class='bx bx-book'></i> Followed Topics</h2>
+        <div id="profile" class="dropdown">
+
+            @if(Auth::check())
+                <button type="button" id="profile-button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class='bx bx-user-circle'></i>
+                    <h2>{{$username}}</h2>
+                </button>
+                <div class="dropdown-menu" aria-labelledby="profile-button">
+                    <a class="dropdown-item" href="{{ route('profile', ['username' => $username]) }}"><h2>See Profile</h2></a>
+                    <a class="dropdown-item" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><h2>Logout</h2></a>
+                    <a class="dropdown-item" href="#"><h2>Something for admin</h2></a>
+                </div>
+                <form id="logout-form" action="{{ route('logout') }}" method="GET" style="display: none;">
+                    @csrf
                 </form>
-            </div>
-        </div>
+            @else
+                <a id="profile-button" href="{{ route('login') }}">
+                    <i class='bx bx-user-circle'></i><h2>Login</h2>
+                </a>
+            @endif
+        </div>  <!-- Needs to be change to get login/logout -->
+    </div>
+    <div id="bottom-part-header">
+        <a href="{{ route('homepage') }}"><h2><i class='bx bx-home-alt'></i> Homepage</h2></a>
+        <a href="{{ route('recentnews.show') }}"><h2><i class='bx bx-stopwatch'></i>Most Recent News</h2></a>
+        <a href="{{ route('votednews.show') }}"><h2><i class='bx bx-sort'></i> Most Voted News</h2></a>
+        <h2><i class='bx bx-trending-up'></i>Trending Tags</h2>
+        <h2 class="topic">
+            <a href="{{ route('search.show', ['topics' => ['Politics']]) }}">Politics</a>
+        </h2>
+        <h2 class="topic">
+            <a href="{{ route('search.show', ['topics' => ['Business']]) }}">Business</a>
+        </h2>
+        <h2 class="topic">
+            <a href="{{ route('search.show', ['topics' => ['Technology']]) }}">Technology</a>
+        </h2>
+        <h2 class="topic">
+            <a href="{{ route('search.show', ['topics' => ['Science']]) }}">Science</a>
+        </h2>
+        <h2><i class='bx bx-news'></i>All Topics</h2>
+        @include('partials.search',['tags' => $tags, 'topics' => $topics])
+    </div>
 </header>
