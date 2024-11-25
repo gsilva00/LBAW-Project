57206826a90dff7c46d6438078c93aa67ebd5b03
Guilherme Silva <gmpas4444@gmail.com>
2024-11-25 18:37:06 +0000

    Fixed admin panel create user

--- resources/views/pages/admin_panel.blade.php
+++ resources/views/pages/admin_panel.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="content">
         <h2>Users:</h2>
-        <form method="POST" action="{{ route('profile.update') }}" enctype="multipart/form-data">
+        <form id="createFullUserForm" method="POST" action="{{ route('adminPanel.createUser') }}" enctype="multipart/form-data">
             @csrf
             <h3>Create New User</h3>
             <div class="profile-info">
@@ -76,38 +76,38 @@
             <br>
             <div class="profile-info">
                 <label for="upvote_notification">
-                    <input type="checkbox" name="upvote_notification" id="upvote_notification" {{ old('upvote_notification', true) ? 'checked' : '' }}>
+                    <input type="checkbox" name="upvote_notification" id="upvote_notification" value="1" {{ old('upvote_notification', true) ? 'checked' : '' }}>
                     <span>Receive Upvote Notifications</span>
                 </label>
             </div>
             <br>
             <div class="profile-info">
                 <label for="comment_notification">
-                    <input type="checkbox" name="comment_notification" id="comment_notification" {{ old('comment_notification', true) ? 'checked' : '' }}>
+                    <input type="checkbox" name="comment_notification" id="comment_notification" value="1" {{ old('comment_notification', true) ? 'checked' : '' }}>
                     <span>Receive Comment Notifications</span>
                 </label>
             </div>
             <br>
             <div class="profile-info">
                 <label for="is_admin">
-                    <input type="checkbox" name="is_admin" id="is_admin" {{ old('is_admin') ? 'checked' : '' }}>
+                    <input type="checkbox" name="is_admin" id="is_admin" value="1" {{ old('is_admin') ? 'checked' : '' }}>
                     <span>Is Admin</span>
                 </label>
             </div>
             <br>
             <div class="profile-info">
                 <label for="is_fact_checker">
-                    <input type="checkbox" name="is_fact_checker" id="is_fact_checker" {{ old('is_fact_checker') ? 'checked' : '' }}>
+                    <input type="checkbox" name="is_fact_checker" id="is_fact_checker" value="1" {{ old('is_fact_checker') ? 'checked' : '' }}>
                     <span>Is Fact Checker</span>
                 </label>
             </div>
             <br>
             <div class="profile-info">
-                <button type="submit" class="large-rectangle small-text greyer">Create</button> <!-- TODO HIGHLIGHT IN RED IF PASSWORD NOT INPUTTED (HARD TO SEE OTHERWISE) -->
+                <button type="submit" class="large-rectangle small-text greyer">Create</button>
             </div>
         </form>
         <br>
-        <div id="users-section" style="height: 800px; overflow-y: scroll;"> <!-- Make the height 2 or 3 users, or more if the user cards are made shorter -->
+        <div id="users-section" style="height: 800px; overflow-y: scroll;">
             <ul id="user-list">
                 @include('partials.user_tile_list', ['users' => $users])
             </ul>
