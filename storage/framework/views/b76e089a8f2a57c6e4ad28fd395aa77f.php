7df261592f748995b86485f7009d7f652d042220
Utilizador <valentinapereira2004@gmail.com>
2024-11-25 15:57:08 +0000

    fixed a small error

---
+++ storage/framework/views/b76e089a8f2a57c6e4ad28fd395aa77f.php
@@ -0,0 +1,9 @@
+<div>
+    <p class="title"><a href="<?php echo e(route('trendingtags')); ?>">Trending Tags</a></p>
+    <nav id="trending-tags-section">
+        <?php $__currentLoopData = $trendingTags; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $tag): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            <p class="smaller-text"><?php echo e($loop->iteration); ?> Trending</p>
+            <span><strong><a href="<?php echo e(route('tag.show', ['name' => $tag->name])); ?>"><?php echo e($tag->name); ?></a></strong></span><?php if(!$loop->last): ?><?php endif; ?>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    </nav>
+</div><?php /**PATH C:\Users\Utiizador\Desktop\LBAW\lbaw24124\resources\views/partials/trending_tags.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/c1b5bde544f7c65cf37504d6c1a09f80.php
@@ -0,0 +1,15 @@
+<div class="news-tile">
+    <a href="<?php echo e(route('article.show', ['id' => $article->id])); ?>">
+        <img src="<?php echo e(asset('images/article/' . $article->article_image)); ?>" alt="News Image">
+        <p class="title"><?php echo e($article->title); ?></p>
+    </a>
+    <?php if(!$article->is_deleted && Auth::check() && Auth::user()->id === $article->author_id): ?>
+    <div class="float-container">
+        <a href="<?php echo e(route('editArticle', ['id' => $article->id])); ?>" class="large-rectangle small-text"><span>Edit</span></a>
+        <form action="<?php echo e(route('deleteArticle', ['id' => $article->id])); ?>" method="POST" style="display:inline;">
+            <?php echo csrf_field(); ?>
+            <?php echo method_field('DELETE'); ?> <!-- HTML forms don't directly support DELETE -->
+            <button type="submit" class="large-rectangle small-text greyer"><span>Delete</span></button>
+        </form>    </div>
+    <?php endif; ?>
+</div><?php /**PATH C:\Users\Utiizador\Desktop\LBAW\lbaw24124\resources\views/partials/news_tile.blade.php ENDPATH**/ ?>
