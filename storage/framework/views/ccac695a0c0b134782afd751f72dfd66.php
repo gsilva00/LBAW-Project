4235ab61dae624f444893ad24d075db951a4c0c4
Guilherme Silva <gmpas4444@gmail.com>
2024-11-25 15:44:24 +0000

    Removed compiled views bloat from .git

--- storage/framework/views/ccac695a0c0b134782afd751f72dfd66.php
+++
@@ -1,7 +0,0 @@
-<div class="recent-news-container">
-    <?php $__currentLoopData = $articles; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $article): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-        <?php echo $__env->make('partials.long_news_tile', [
-            'article' => $article,
-        ], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-</div><?php /**PATH C:\Users\Utiizador\Desktop\LBAW\lbaw24124\resources\views/partials/articles_list.blade.php ENDPATH**/ ?>
