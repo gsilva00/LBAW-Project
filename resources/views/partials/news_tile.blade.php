121ce70ff0e1dad0f30dd67cd03055c0cd4e55a6
Guilherme Silva <gmpas4444@gmail.com>
2024-11-25 16:13:52 +0000

    WIP commit for Valentina to work

--- resources/views/partials/news_tile.blade.php
+++ resources/views/partials/news_tile.blade.php
@@ -8,8 +8,8 @@
         <a href="{{ route('editArticle', ['id' => $article->id]) }}" class="large-rectangle small-text"><span>Edit</span></a>
         <form action="{{ route('deleteArticle', ['id' => $article->id]) }}" method="POST" style="display:inline;">
             @csrf
-            @method('DELETE') <!-- HTML forms don't directly support DELETE -->
             <button type="submit" class="large-rectangle small-text greyer"><span>Delete</span></button>
-        </form>    </div>
+        </form>
+    </div>
     @endif
 </div>
