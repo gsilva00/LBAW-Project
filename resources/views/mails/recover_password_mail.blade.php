a2f8656f91d9bdab30394991f301332fa59a45f8
Gabriel Carvalho <up202208939@edu.fe.up.pt>
2024-12-19 10:18:13 +0000

    Added css and laravel template to email

--- resources/views/mails/recover_password_mail.blade.php
+++ resources/views/mails/recover_password_mail.blade.php
@@ -1,9 +1,16 @@
-<h3>NewFlow Password Recovery</h3>
-<h4>Hello {{ $username }}!</h4>
-<h4>We received a password recovery request for your NewFlow account. If this request was not made by you, consider changing your password.</h4>
+@component('mail::message')
+
+<h1>NewFlow Password Recovery</h1>
+<h2>Hello {{ $username }}!</h2>
+<p>We received a password recovery request for your NewFlow account. If this request was not made by you, consider changing your password.</p>
+
+<div style="border: 2px solid #393939; padding: 10px; border-radius: 5px; text-align: center;">
+    <h4 style="margin: 0;">Your verification code:</h4>
+    <h4 style="margin: 0;">{{ $code }}</h4>
+</div>
+
 <br>
-<h4>Your verification code:</h4>
-<h4>{{ $code }}</h4>
-<br>
-<h5>-------</h5>
-<h5>Overflow Incorporate</h5>
+<p style="margin: 0;">Regards,</p>
+<h4 style="margin: 0;">Overflow Inc.</h4>
+
+@endcomponent
