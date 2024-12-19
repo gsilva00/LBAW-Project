ffca734986fddcfdd675c007ecff4d257379db60
Gabriel Carvalho <up202208939@edu.fe.up.pt>
2024-12-19 10:47:46 +0000

    Updated email for better accessibility

--- resources/views/mails/recover_password_mail.blade.php
+++ resources/views/mails/recover_password_mail.blade.php
@@ -5,12 +5,12 @@
 <p>We received a password recovery request for your NewFlow account. If this request was not made by you, consider changing your password.</p>
 
 <div style="border: 2px solid #393939; padding: 10px; border-radius: 5px; text-align: center;">
-    <h4 style="margin: 0;">Your verification code:</h4>
-    <h4 style="margin: 0;">{{ $code }}</h4>
+    <p style="margin: 0; text-align: center;"><b>Your verification code:</b></p>
+    <p style="margin: 0; text-align: center;"><b>{{ $code }}</b></p>
 </div>
 
 <br>
 <p style="margin: 0;">Regards,</p>
-<h4 style="margin: 0;">Overflow Inc.</h4>
+<p style="margin: 0;"><b>Overflow Inc.</b></p>
 
 @endcomponent
